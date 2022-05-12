9da174960ef616d5db8a3383e37f8fc04ee57a70
nurzhanx <nwrzhans@gmail.com>
2022-05-12 23:22:32 +0200

    make separate brain-game and brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,29 +1,8 @@
-import readlineSync from 'readline-sync';
+import helloUser from '../src/cli.js';
+import brainEvenGame from '../src/brainEvenGame.js';
 
-const brainEvenGame = (userName) => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let winOrLoose = true;
+console.log('Welcome to the Brain Games!');
 
-  for (let i = 0; i < 3; i += 1) {
-    const number = Math.floor(Math.random() * 100);
-    console.log(`Question: ${number}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    const correctAnswer = (number % 2 === 0) ? 'yes' : 'no';
+const userName = helloUser();
 
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer is '${correctAnswer}'`);
-      winOrLoose = false;
-      break;
-    }
-  }
-
-  if (winOrLoose) {
-    console.log(`Congratulations, ${userName}!`);
-  } else {
-    console.log(`Let's try again, ${userName}!`);
-  }
-};
-
-export default brainEvenGame;
+brainEvenGame(userName);
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
 import helloUser from '../src/cli.js';
-import brainEvenGame from './brain-even.js';
 
 console.log('Welcome to the Brain Games!');
 
-const userName = helloUser();
-
-brainEvenGame(userName);
+helloUser();
