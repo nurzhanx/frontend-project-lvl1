0c6a11b133424cc13fe470060b0a53ffeae43d05
nurzhanx <nwrzhans@gmail.com>
2022-05-13 01:28:22 +0200

    add game brainCalc

---
+++ src/brainCalc.js
@@ -0,0 +1,40 @@
+import gameLogic from './gameLogic.js';
+import welcomeUser from './cli.js';
+
+const brainCalc = () => {
+  const playerName = welcomeUser();
+  const introMessage = 'What is the result of the expression?';
+  const pairs = [];
+
+  for (let i = 0; i < 3; i += 1) {
+    const pair = [];
+    const x = Math.floor(Math.random() * 100);
+    const y = Math.floor(Math.random() * 100);
+    const operators = ['+', '-', '*'];
+    const operator = operators[Math.floor(Math.random() * operators.length)];
+    const question = `${x} ${operator} ${y}`;
+    let correctAnswer;
+
+    switch (operator) {
+      case '+':
+        correctAnswer = x + y;
+        break;
+      case '-':
+        correctAnswer = x - y;
+        break;
+      case '*':
+        correctAnswer = x * y;
+        break;
+      default:
+        break;
+    }
+
+    pair.push(question);
+    pair.push(String(correctAnswer));
+    pairs.push(pair);
+  }
+
+  gameLogic(playerName, introMessage, pairs);
+};
+
+export default brainCalc;
