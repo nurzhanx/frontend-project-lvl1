8ddaa8d9d0118421c9d4c6cebc213f188822744f
nurzhanx <nwrzhans@gmail.com>
2022-05-12 18:24:10 +0200

    add function saying hello to user

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const helloUser = () => {
+	const name = readlineSync.question('May I have your name? ');
+	
+	console.log(`Hello, ${name}!`);
+};
+
+export { helloUser };
